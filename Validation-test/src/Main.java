db7606169245f1d0395ad5a1add271315c784691
Ayobami <ayobamiadepoju263@gmail.com>
2025-10-01 18:19:53 +0100

    changes in main

--- Validation-test/src/Main.java
+++ Validation-test/src/Main.java
@@ -5,39 +5,19 @@ class Main {
     public static void main(String[] args) {
         User user1 = new User("John", "Doe", "johndoe@gmail.com");
         User user2 = new User("Matthew", "Lad", "alice.com");
-        User user3 = new User("Alice","Jude", "alicejude@gmail.com");
+        User user3 = new User(null,"Jude", "alicejude@gmail.com");
         User user4 = new User("", "", "");
 
-        try{
-            Validator.validate(user1);
-            System.out.println(user1.getFirstName() + " " + user1.getLastName() + " " + user1.getEmail());
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println();
-
-        try {
-            Validator.validate(user2);
-            System.out.println(user2.getFirstName() + " " + user2.getLastName() + " " + user2.getEmail());
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println();
-
-        try {
-            Validator.validate(user3);
-            System.out.println(user3.getFirstName() + " " + user3.getLastName() + " " + user3.getEmail());
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println();
-
-        try {
-            Validator.validate(user4);
-            System.out.println(user4.getFirstName() + " " + user4.getLastName() + " " + user4.getEmail());
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
+        User[] users = {user1, user2, user3, user4};
+        for (User user : users) {
+            System.out.println("Validating User ...");
+            try {
+                Validator.validate(user);
+                System.out.println(user.getFirstName() + " " + user.getLastName() + " " + user.getEmail());
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println();
         }
     }
 }
--- Validation-test/src/annotations/NotNull.java
+++ Validation-test/src/annotations/NotNull.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotNull {
 
-    String message() default "Field is required";
+    String message() default "Field must not be null";
 }
